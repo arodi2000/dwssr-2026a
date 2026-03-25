f1050b173c7ac9c9afab28bba1fb69f48d6688d9
arodi2000 <105835438+arodi2000@users.noreply.github.com>
2026-03-25 16:55:54 +0000

    🔧chore:vit configuration en proceso

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+//importo la funcion de configuracion de vite
+import {defineConfig}from`vite`
+//importo un resolverdor de rutas
+import{resolve}from"node.path"
+
+//exportar una instacias de configuracion
+export default defineConfig({})
